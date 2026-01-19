f98b855610b8067de2d92691ffb66640a755ff49
Gaya Vish <gayanthavi002@gmail.com>
2026-01-19 11:33:05 +0530

    changes2

--- todo_backend/main.py
+++ todo_backend/main.py
@@ -3,9 +3,9 @@ from typing import List
 import models
 from fastapi import Body, FastAPI
 
-from todo_backend.core.database import engine
-from todo_backend.routers.taskrouters import router as task_router
-from todo_backend.routers.userrouters import router as user_router
+from ..todo_backend.core.database import engine
+from ..todo_backend.routers.taskrouters import router as task_router
+from ..todo_backend.routers.userrouters import router as user_router
 
 app = FastAPI()
 app.include_router(task_router)
