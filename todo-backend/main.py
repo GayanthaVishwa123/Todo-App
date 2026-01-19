8d93caf187bd09323c58f4f6a45e0ecc3db8facf
Gaya Vish <gayanthavi002@gmail.com>
2026-01-19 10:54:01 +0530

    create init.py file

--- todo-backend/main.py
+++ todo-backend/main.py
@@ -1,7 +1,9 @@
 from typing import List
 
+import models
 from fastapi import Body, FastAPI
 
+from .core.database import Engine
 from .routers.taskrouters import router as task_router
 from .routers.userrouters import router as user_router
 
@@ -13,3 +15,6 @@ app.include_router(user_router)
 @app.get("/")
 async def root():
     return {"message": "Todo API is running 🚀"}
+
+
+models.Base.metadata.create_all(bind=Engine)
