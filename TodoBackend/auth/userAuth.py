eafb42362a0ef2d5e5734e9731dad250d565b049
Gaya Vish <gayanthavi002@gmail.com>
2026-01-22 21:35:31 +0530

    create jwt and token

--- TodoBackend/auth/userAuth.py
+++ TodoBackend/auth/userAuth.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from ..auth.passwordAuth import passwordVeryfied
@@ -9,6 +12,14 @@ from ..core.database import get_db
 from ..models.todo import User
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+SECRET_KEY = "df08a3baaa2ee64e49006c4a33c848fa6cc2176e960a1ad21a1e1cac66c53499"
+ALGORITHUM = "HA256"
+
+
+class AcessToken(BaseModel):
+    acsess_token: str
+    token_type: str
 
 
 def userAuthenticate(user_name: str, password: str, db: db_dependency):
@@ -20,6 +31,24 @@ def userAuthenticate(user_name: str, password: str, db: db_dependency):
     return True
 
 
+def cretae_token(username: str, user_id: int, expireTime: timedelta):
+    enacode = {"sub": username, "id": user_id}
+    expiretime = datetime.utcnow() + expireTime
+    # enacode['exp']=expiretime
+    enacode.update({"exp": expiretime})
+
+    return jwt.enacode(enacode, SECRET_KEY, algorithum=ALGORITHUM)
+
+def login_acsess():
+    
 
 
-def login_access():
+
+def loginaccsess_token(
+    form: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = userAuthenticate(form.username, form.password, db)
+    if not user:
+        return {"mesage"}
+    token = cretae_token(user.username, user.id, timedelta(minutes=20))
+    return token
