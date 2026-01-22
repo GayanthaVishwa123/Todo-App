058119c63564c42a0369afc4fd727760f322c110
Gaya Vish <gayanthavi002@gmail.com>
2026-01-22 09:38:23 +0530

    has pasword created

--- TodoBackend/auth/passwordAuth.py
+++ TodoBackend/auth/passwordAuth.py
@@ -1,1 +1,13 @@
+from passlib.context import CryptContext
 
+password_context = CryptContext(schemes=["bcrypt"], Deprecated="auto")
+
+
+# hash password
+def hashpassword(passwprd: str):
+    return password_context.hash(passwprd)
+
+
+# password veryfied
+def passwordVeryfied(plain_password: str, hashed_password: str):
+    return password_context.verify(plain_password, hashed_password)
