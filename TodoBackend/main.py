ddf2f6eccc2470638b1b8c8229227a0c62a8ce3e
Gaya Vish <gayanthavi002@gmail.com>
2026-01-26 18:38:44 +0530

    add commit

--- TodoBackend/main.py
+++ TodoBackend/main.py
@@ -3,12 +3,11 @@ from fastapi import FastAPI
 from TodoBackend.models import todo
 
 from .core.database import Base, engine
-
-# from .routers.taskrouters import router as task_router
+from .routers.taskrouters import router as task_router
 from .routers.userrouters import router as user_router
 
 app = FastAPI()
-# app.include_router(task_router)
+app.include_router(task_router)
 app.include_router(user_router)
 
 
